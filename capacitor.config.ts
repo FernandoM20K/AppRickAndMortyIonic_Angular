d94735b97ea0f814871df74cb285e52e607337b0
Fernando Moron <luisfernandomoron.espinoza@gmail.com>
2023-04-11 10:28:28 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RickAndMortyIonic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
